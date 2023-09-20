eec50185c991c8f67091c75f3b9d36c8ab5f2802
SALA COMPUTO SEDE CENTRO - BARRANQUILLA <salacomputocentro.baq@unilibre.edu.co>
2023-09-20 11:12:57 -0500

    se creo el api de ruta

---
+++ api/Ruta.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, jsonify
+
+from models.Ruta import Ruta, RutaSchema
+
+ruta_ruta = Blueprint("ruta_ruta",__name__)
+#routes_cliente = Blueprint("routes_cliente", __name__)
+
+ruta_schema = RutaSchema()
+rutas_schema = RutaSchema(many=True)
+
+@ruta_ruta.route("/rutas", methods=["GET"])
+def ruta():
+    resultall = Ruta.query.all()# Select * from Ruta;
+    result = rutas_schema.dump(resultall)
+    return jsonify(result)
